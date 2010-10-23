b9cb8512d0838047f89f72cd2286726db98207b7
kovikti@gmail.com <kovikti@gmail.com@573e1637-90a8-5817-492c-ced4b4163f8c>
2010-10-23 16:06:13 +0000

    Start - WinPho7 can call Webservice hosted by WPF app, interaction between service, host app... Maybe remoting instead of WS?

---
+++ trunk/MMProgWP7/MMProgServiceLib/MyServiceHost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace MMProgServiceLib
+{
+    public class MyServiceHost:ServiceHost
+    {
+        public MyServiceHost(Type serviceType, params Uri[] baseAddresses)
+            : base(serviceType, baseAddresses)
+        {
+
+        }
+        public void FireEvent(string eventName)
+        {
+            if (EventFired != null)
+            {
+                EventFired.BeginInvoke(eventName, 
+                    delegate(IAsyncResult iar){
+                        EventFired.EndInvoke(iar);
+                    }
+                    , null);
+                
+            }
+        }
+        public event Action<string> EventFired; 
+        
+    }
+}
---
+++ trunk/MMProgWP7/WPClient/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WPClient.MMProgServiceReference;
+
+
+
+namespace WPClient
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MMProgServiceClient client = new MMProgServiceClient();
+            client.SendMessageToServerCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_SendMessageToServerCompleted);
+            client.SendMessageToServerAsync(new MyMessage() { Owner = "Me", Text = "Message text" });
+            
+        }
+
+        void client_SendMessageToServerCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            (sender as MMProgServiceClient).CloseAsync();
+        }
+    }
+}
